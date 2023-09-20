eb783857efd1cb87b781c6c5c397bfd8e81e5736
Lans12345 <olanalans12345@gmail.com>
2023-09-20 15:00:46 +0800

    added main home screen

--- lib/screens/main_home_screen.dart
+++ lib/screens/main_home_screen.dart
@@ -1,14 +1,51 @@
 import 'package:animal_rescue/utils/colors.dart';
-import 'package:animal_rescue/widgets/button_widget.dart';
+import 'package:animal_rescue/widgets/drawer_widget.dart';
 import 'package:animal_rescue/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
-class MainHomeScreen extends StatelessWidget {
+class MainHomeScreen extends StatefulWidget {
   const MainHomeScreen({super.key});
+
+  @override
+  State<MainHomeScreen> createState() => _MainHomeScreenState();
+}
+
+class _MainHomeScreenState extends State<MainHomeScreen> {
+  List tabs = [
+    const SizedBox(),
+    const SizedBox(),
+    const SizedBox(),
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const DrawerWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.white,
+        child: Icon(
+          Icons.add,
+          color: primary,
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: primary,
+        title: TextWidget(
+          text: 'Animal Rescue',
+          fontSize: 18,
+          color: Colors.white,
+        ),
+        centerTitle: true,
+      ),
       body: Container(
         height: double.infinity,
         width: double.infinity,
@@ -42,15 +79,38 @@ class MainHomeScreen extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
-              ButtonWidget(
-                color: Colors.white,
-                textColor: Colors.green,
-                label: 'Get Started',
-                onPressed: () {},
-              ),
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedLabelStyle: const TextStyle(fontFamily: 'Bold', fontSize: 10),
+        unselectedLabelStyle: const TextStyle(fontFamily: 'Bold', fontSize: 10),
+        selectedItemColor: primary,
+        unselectedItemColor: Colors.grey[400],
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: _currentIndex != 0
+                ? const Icon(Icons.home_outlined)
+                : const Icon(Icons.home_rounded),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: _currentIndex != 1
+                ? const Icon(Icons.message_outlined)
+                : const Icon(Icons.message),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: _currentIndex != 1
+                ? const Icon(Icons.person_3_outlined)
+                : const Icon(Icons.person_3),
+            label: 'Favorites',
+          ),
+        ],
       ),
     );
   }
